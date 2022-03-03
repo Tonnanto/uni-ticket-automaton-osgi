1d1f1fc76c61809fea5fd8db5188feeee4a63e59
Plploe <jonas.harms@stud.leuphana.de>
2022-03-03 18:54:21 +0100

    paar Anpassungen, glaube ich

--- component-service-bus/src/main/java/de/leuphana/cosa/componentservicebus/behaviour/ComponentServiceBus.java
+++ component-service-bus/src/main/java/de/leuphana/cosa/componentservicebus/behaviour/ComponentServiceBus.java
@@ -168,10 +168,11 @@ public class ComponentServiceBus implements BundleActivator, EventHandler {
 
             case DocumentService.DOCUMENT_CREATED_TOPIC:
                 documentToPrintableAdapter.onDocumentCreated(event);
+                printReportToSendableAdapter.onDocumentCreated(event);
                 break;
 
             case PrintingService.PRINT_REPORT_CREATED_TOPIC:
-                // TODO: delegate to adapter
+                printReportToSendableAdapter.onPrintReportCreated(event);
                 break;
 
             default:
--- component-service-bus/src/main/java/de/leuphana/cosa/componentservicebus/behaviour/adapter/PrintReportToSendableAdapter.java
+++ component-service-bus/src/main/java/de/leuphana/cosa/componentservicebus/behaviour/adapter/PrintReportToSendableAdapter.java
@@ -1,5 +1,7 @@
 package de.leuphana.cosa.componentservicebus.behaviour.adapter;
 
+import de.leuphana.cosa.documentsystem.behaviour.service.DocumentService;
+import de.leuphana.cosa.documentsystem.structure.TicketDocumentTemplate;
 import de.leuphana.cosa.messagingsystem.behaviour.service.MessagingService;
 import de.leuphana.cosa.messagingsystem.structure.Sendable;
 import de.leuphana.cosa.printingsystem.behaviour.service.PrintingService;
@@ -9,16 +11,23 @@ import org.osgi.service.event.Event;
 
 public class PrintReportToSendableAdapter {
     MessagingService messagingService;
+    private TicketDocumentTemplate ticketDocument;
 
     public PrintReportToSendableAdapter(MessagingService messagingService) {
         this.messagingService = messagingService;
+    }
+
+    public  void onDocumentCreated(Event event) {
+        if(event.getProperty(DocumentService.DOCUMENT_KEY) instanceof TicketDocumentTemplate ticketDocument) {
+            this.ticketDocument = ticketDocument;
+        }
     }
 
     public void onPrintReportCreated(Event event) {
         // convert to sendable
         Sendable sendable = new Sendable();
         if(event.getProperty(PrintingService.PRINT_KEY) instanceof PrintReport printReport) {
-            sendable.setContent(printReport.getTicketName());
+            sendable.setContent(ticketDocument.getDocument());
         }
         // hand sendable to messagingService for logging
         messagingService.sendMessage(sendable);
