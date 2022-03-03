1bf1c8f0aa7f16adc559b370188136b30c2dc751
Plploe <jonas.harms@stud.leuphana.de>
2022-03-03 18:28:35 +0100

    Ich raff die adapter noch nicht so 100%tig. Lass darüber morgen mal Reden

--- component-service-bus/src/main/java/de/leuphana/cosa/componentservicebus/behaviour/adapter/PrintReportToSendableAdapter.java
+++ component-service-bus/src/main/java/de/leuphana/cosa/componentservicebus/behaviour/adapter/PrintReportToSendableAdapter.java
@@ -1,8 +1,11 @@
 package de.leuphana.cosa.componentservicebus.behaviour.adapter;
 
 import de.leuphana.cosa.messagingsystem.behaviour.service.MessagingService;
+import de.leuphana.cosa.messagingsystem.structure.Sendable;
+import de.leuphana.cosa.printingsystem.behaviour.service.PrintingService;
 import de.leuphana.cosa.printingsystem.structure.PrintReport;
 import org.osgi.framework.BundleContext;
+import org.osgi.service.event.Event;
 
 public class PrintReportToSendableAdapter {
     MessagingService messagingService;
@@ -11,8 +14,13 @@ public class PrintReportToSendableAdapter {
         this.messagingService = messagingService;
     }
 
-    public void onPrintReportCreated(PrintReport printReport) {
+    public void onPrintReportCreated(Event event) {
         // convert to sendable
+        Sendable sendable = new Sendable();
+        if(event.getProperty(PrintingService.PRINT_KEY) instanceof PrintReport printReport) {
+            sendable.setContent(printReport.getTicketName());
+        }
         // hand sendable to messagingService for logging
+        messagingService.sendMessage(sendable);
     }
 }
--- printing-system/src/main/java/de/leuphana/cosa/printingsystem/behaviour/service/PrintingService.java
+++ printing-system/src/main/java/de/leuphana/cosa/printingsystem/behaviour/service/PrintingService.java
@@ -4,5 +4,6 @@ import de.leuphana.cosa.printingsystem.structure.Printable;
 
 public interface PrintingService {
     String PRINT_REPORT_CREATED_TOPIC = "printingservice/printreport/created";
+    String PRINT_KEY = "print";
     void printPrintable(Printable printable);
 }
