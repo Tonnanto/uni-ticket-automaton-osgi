9cc1c9a35aa386ce92560a3f60fef9db8699f935
Plploe <jonas.harms@stud.leuphana.de>
2022-03-03 17:49:40 +0100

    MessageingSystem angepasst.

--- messaging-system/src/main/java/de/leuphana/cosa/messagingsystem/behaviour/MessagingServiceImpl.java
+++ messaging-system/src/main/java/de/leuphana/cosa/messagingsystem/behaviour/MessagingServiceImpl.java
@@ -2,10 +2,13 @@ package de.leuphana.cosa.messagingsystem.behaviour;
 
 import de.leuphana.cosa.messagingsystem.behaviour.service.MessagingService;
 import de.leuphana.cosa.messagingsystem.structure.DeliveryReport;
+import de.leuphana.cosa.messagingsystem.structure.MessageType;
 import de.leuphana.cosa.messagingsystem.structure.Sendable;
 import de.leuphana.cosa.messagingsystem.structure.message.Message;
 import de.leuphana.cosa.messagingsystem.structure.messagingfactory.AbstractMessagingFactory;
 import de.leuphana.cosa.messagingsystem.structure.messagingprotocol.MessagingProtocol;
+import de.leuphana.cosa.uisystem.structure.SelectionView;
+import de.leuphana.cosa.uisystem.structure.StringView;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.osgi.framework.BundleActivator;
@@ -13,58 +16,104 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
 public class MessagingServiceImpl implements MessagingService, BundleActivator {
 
-	private ServiceReference<MessagingService> reference;
-	private ServiceRegistration<MessagingService> registration;
+    private ServiceReference<MessagingService> reference;
+    private ServiceRegistration<MessagingService> registration;
 
-	private Logger logger;
+    private Logger logger;
 
-	@Override
-	public void start(BundleContext bundleContext) {
-		System.out.println("Registering MessagingService.");
-		registration = bundleContext.registerService(
-				MessagingService.class,
-				this,
-				new Hashtable<String, String>());
-		reference = registration
-				.getReference();
+    @Override
+    public void start(BundleContext bundleContext) {
+        System.out.println("Registering MessagingService.");
+        registration = bundleContext.registerService(
+                MessagingService.class,
+                this,
+                new Hashtable<String, String>());
+        reference = registration
+                .getReference();
 
-	}
+    }
 
-	@Override
-	public void stop(BundleContext bundleContext) {
-		System.out.println("Unregistering MessagingService.");
-		registration.unregister();
-	}
+    @Override
+    public void stop(BundleContext bundleContext) {
+        System.out.println("Unregistering MessagingService.");
+        registration.unregister();
+    }
 
-	@Override
-	public DeliveryReport sendMessage(Sendable sendable) {
-		logger = LogManager.getLogger(this.getClass());
-		
-		AbstractMessagingFactory abstractMessagingFactory = AbstractMessagingFactory.getFactory(sendable.getMessageType());
+    @Override
+    public DeliveryReport sendMessage(Sendable sendable) {
+        logger = LogManager.getLogger(this.getClass());
 
-		Message message = abstractMessagingFactory.createMessage(sendable.getSender(), sendable.getReceiver(), sendable.getContent());
+        SelectionView selectionView = new SelectionView() {
+            @Override
+            protected List<String> getOptions() {
+                List<String> option = new ArrayList<>();
+                option.add("Keine Quittung");
+                option.add("via E-Mail");
+                option.add("via SMS");
+                return option;
+            }
 
-		MessagingProtocol messageProtocol = abstractMessagingFactory.createMessagingProtocol();
-		messageProtocol.open();
-		messageProtocol.transfer(message);
-		messageProtocol.close();
+            @Override
+            protected String getMessage() {
+                return "Möchten Sie eine Quittung zugestellt bekommen?";
+            }
+        };
+        int selectedIndex = selectionView.displaySelection();
+        if (selectedIndex != 0) {
+            switch (selectedIndex) {
+                case 1:
+                    sendable.setMessageType(MessageType.EMAIL);
+                    sendable.setSender("ticket@automat.de");
+                    StringView stringView = new StringView() {
+                        @Override
+                        protected String getMessage() {
+                            return "Geben Sie ihre E-Mail Addresse ein: ";
+                        }
+                    };
+                    sendable.setReceiver(stringView.readIntInput());
+                    break;
+                case 2:
+                    sendable.setMessageType(MessageType.SMS);
+                    sendable.setSender("0152242069");
+                    stringView = new StringView() {
+                        @Override
+                        protected String getMessage() {
+                            return "Geben Sie ihre Handynummer ein: ";
+                        }
+                    };
+                    sendable.setReceiver(stringView.readIntInput());
+                    break;
+                default:
+                    break;
+            }
+            AbstractMessagingFactory abstractMessagingFactory = AbstractMessagingFactory.getFactory(sendable.getMessageType());
 
-		logger.info("Message: " + sendable.getContent() + " transported via " + sendable.getMessageType());
+            Message message = abstractMessagingFactory.createMessage(sendable.getSender(), sendable.getReceiver(), sendable.getContent());
 
-		DeliveryReport deliveryReport = new DeliveryReport();
-		deliveryReport.setSender(sendable.getSender());
-		deliveryReport.setReceiver(sendable.getReceiver());
-		deliveryReport.setContent(sendable.getContent());
-		deliveryReport.setMessageType(sendable.getMessageType().toString());
+            MessagingProtocol messageProtocol = abstractMessagingFactory.createMessagingProtocol();
+            messageProtocol.open();
+            messageProtocol.transfer(message);
+            messageProtocol.close();
+
+            logger.info("Message: " + sendable.getContent() + " transported via " + sendable.getMessageType());
+
+            DeliveryReport deliveryReport = new DeliveryReport();
+            deliveryReport.setSender(sendable.getSender());
+            deliveryReport.setReceiver(sendable.getReceiver());
+            deliveryReport.setContent(sendable.getContent());
+            deliveryReport.setMessageType(sendable.getMessageType().toString());
 
 //		for (MessagingEventListener listener: listeners) {
 //			listener.onMessageSent(new MessagingEvent(deliveryReport));
 //		}
 
-		return deliveryReport;
-	}
+            return deliveryReport;
+        } else return null;
+    }
 }
